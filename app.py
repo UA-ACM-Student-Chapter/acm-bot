b1c601593a3836e3231385b6a12fbb861b8402a6
Mayank <a7x.mayank@gmail.com>
2018-02-26 19:26:53 -0600

    Create basic flask app

---
+++ app.py
@@ -0,0 +1,32 @@
+import json
+import os
+import random
+import sys
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from flask import Flask, request
+from slackclient import SlackClient
+
+app = Flask(__name__)
+slack_token = os.environ["SLACK_BOT_TOKEN"]
+sc = SlackClient(slack_token)
+
+
+# Webhook for all requests
+@app.route('/', methods=['POST'])
+def webhook():
+  data = request.get_json()
+  log('Recieved {}'.format(data))
+  return "ok", 200
+
+
+# Simple wrapper for sending a Slack message
+def send_slack_message(channel, message):
+  return sc.api_call(
+    "chat.postMessage",
+    channel=channel,
+    text=message
+  )
+
+# send_slack_message('#webteam', 'Hi')
