6223df7bfe35e705862f6d9a995ecd469c274f6c
mayank99 <a7x.mayank@gmail.com>
2018-03-05 20:39:46 -0600

    add bot name validation

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ def webhook():
   flag = True
   if 'subtype' in event:
     flag = event['subtype'] not in ['bot_message', 'message_changed']
+  if 'username' in event:
+    flag = flag and event['username'] == bot_name
   if event['type'] == 'message' and flag:
     text = str(event.get('text')).lower()
     if 'shirt' in text or 'size' in text:
