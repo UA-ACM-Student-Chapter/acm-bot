689ae6c11ad0d77852f44fbe2a5167430387f69c
David <damccoy1@crimson.ua.edu>
2018-11-08 09:27:50 -0600

    added print

--- app.py
+++ app.py
@@ -316,6 +316,8 @@ def get_db_connection():
   return client.heroku_j9g2w0v4
 
 def is_admin(user):
+  print(user)
+  print(get_email(user))
   return get_email(user) in os.environ["ADMIN"].split(" ")
 
 def prompt_elections_list(channel):
